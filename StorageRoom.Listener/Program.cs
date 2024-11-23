e08fa58e891483a5c08f0c40f13431e9d4e608ab
gfi406 <makafanasiev@gmail.com>
2024-11-23 23:38:28 +0300

    Добавьте файлы проекта.

---
+++ StorageRoom.Listener/Program.cs
@@ -0,0 +1,27 @@
+﻿using StorageRoom.Messages;
+using Microsoft.Extensions.Configuration;
+using EasyNetQ;
+using System;
+
+var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+
+var amqp = config.GetConnectionString("AutoRabbitMQ");
+using var bus = RabbitHutch.CreateBus(amqp);
+Console.WriteLine("Подключился!");
+
+var subscriberId = $"Listener@{Environment.MachineName}";
+Console.WriteLine("Слушаю RabbitMQ. Жду сообщения...");
+
+bus.Subscribe<Message>("my_queue", HandleMessage);
+
+Console.ReadLine();
+
+// Метод для обработки сообщений
+void HandleMessage(Message message)
+{
+    Console.WriteLine($"Получено сообщение: {message.Content}");
+    // Здесь можно добавить любую логику для обработки полученного сообщения
+}
